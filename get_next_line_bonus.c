33ec0826a77e39cf07623bd024e060f6ebd47bda
Romane LESEUR <rleseur@e2r12p20.clusters.42paris.fr>
2021-12-10 17:05:14 +0100

    correcton (attention malloc ??)

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -1,16 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rleseur <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 12:48:26 by rleseur           #+#    #+#             */
-/*   Updated: 2021/12/10 16:51:41 by rleseur          ###   ########.fr       */
+/*   Updated: 2021/12/10 16:47:27 by rleseur          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 int	get_read(int fd, char **mem)
 {
@@ -77,7 +77,7 @@ char	*get_next_line(int fd)
 	return (str);
 }
 
-int	main(int ac, char **av)
+/*int	main(int ac, char **av)
 {
 	char	*str;
 	int		fd;
@@ -90,4 +90,4 @@ int	main(int ac, char **av)
 		free(str);
 	}
 	return (0);
-}
+}*/
---
+++ get_next_line_bonus.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rleseur <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/10 12:47:49 by rleseur           #+#    #+#             */
+/*   Updated: 2021/12/10 16:44:24 by rleseur          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# include <stdio.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+
+int		get_read(int fd, char **mem);
+char	*get_line(char **str, char **mem);
+char	*get_next_line(int fd);
+
+int		ft_strlen(char *s);
+char	*ft_strdup(char *s);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(char *s, int c);
+char	*ft_substr(char *s, unsigned int start, size_t len);
+
+#endif
